bcb279d7a3b49aadfd4e45c2055e08d5e8a02117
BA <engineervinceblair@gmail.com>
2020-04-13 10:29:16 +0300

    logics that get username, repos, followers, blog, email

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { GithubService } from "../github-service/github.service";
 
 @Component({
-  selector: 'app-landing',
-  templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.css']
+  selector: "app-landing",
+  templateUrl: "./landing.component.html",
+  styleUrls: ["./landing.component.css"]
 })
 export class LandingComponent implements OnInit {
+  user: any;
+  repos: any;
+  username: any;
+  constructor(private githubService: GithubService) {
+    this.githubService.getUser().subscribe(user => {
+      this.user = user;
+      console.log(this.user);
+    });
+    this.githubService.getRepos().subscribe(repos => {
+      this.repos = repos;
+      console.log(this.repos);
+    });
+  }
+  search() {
+    this.githubService.updateUsername(this.username);
 
-  constructor() { }
-
-  ngOnInit(): void {
+    this.githubService.getUser().subscribe(user => {
+      this.user = user;
+      console.log(this.user);
+    });
+    this.githubService.getRepos().subscribe(repos => {
+      this.repos = repos;
+      console.log(this.repos);
+    });
   }
 
+  ngOnInit() {}
 }
