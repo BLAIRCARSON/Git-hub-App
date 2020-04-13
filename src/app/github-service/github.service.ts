037683e54d7720d1047e6ca1ed23c89b54aebad4
BA <engineervinceblair@gmail.com>
2020-04-13 10:30:49 +0300

    using http.get to get user, repos

--- src/app/github-service/github.service.ts
+++ src/app/github-service/github.service.ts
@@ -1,9 +1,40 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class GithubService {
+  private username = "BLAIRCARSON";
+  private client_id = "Iv1.14523a4f7a9ab016";
+  private client_secret = "6c61519b4b2a63b0f6c7f4bded4fe4afd63af41f";
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    console.log("Github service started");
+  }
+  getUser() {
+    return this.http.get(
+      "https://api.github.com/users/" +
+      this.username +
+      "?client_id=" +
+      this.client_id +
+      "&client_secret=" +
+      this.client_secret
+    );
+  }
+  getRepos() {
+    return this.http.get(
+      "https://api.github.com/users/" +
+      this.username +
+      "/repos" +
+      "?client_id=" +
+      this.client_id +
+      "&client_secret=" +
+      this.client_secret
+    );
+  }
+  updateUsername(username: any) {
+    this.username = username;
+  }
 }
